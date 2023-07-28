16aa7c5ec63fae6a328977681b93b54db0346351
Kogay Artem <kogay10987@gmail.com>
2023-07-28 20:27:14 +0500

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/components/Footer";
+import { styled } from "styled-components";
 
 function App() {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
+  return (
+    <Container>
+      <Header />
+      <Center></Center>
+      <Footer />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100%;
+`;
+const Center = styled.div`
+  width: 100%;
+  min-height: calc(100vh - 110px);
+`;
 
 export default App;
