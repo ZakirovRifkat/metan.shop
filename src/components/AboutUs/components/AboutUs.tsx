b7834d0acf89100845d2a3952c6ade24bfadd0fe
Zakirov Rifkat <zakirov2323rifkat@gmail.com>
2023-08-04 18:03:34 +0300

    add normal animation to about us page

--- src/components/AboutUs/components/AboutUs.tsx
+++ src/components/AboutUs/components/AboutUs.tsx
@@ -1,34 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { css, styled } from "styled-components";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { GalleryItem } from "../../GalleryItem/GalleryItem";
 import logo from "../assets/logo.svg";
 import pic1 from "../assets/pic1.jpg";
 import pic2 from "../assets/pic2.jpg";
 import pic3 from "../assets/pic3.jpg";
 import pic4 from "../assets/pic4.jpg";
-import "./anim.css";
 
 export const AboutUs = () => {
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const images1 = [pic4, pic2, pic1, pic3];
     const images2 = [pic1, pic3, pic2, pic4];
     const images3 = [pic3, pic1, pic4, pic2];
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            console.log();
 
-            setCurrentImageIndex((prev) => (prev + 1) % images1.length);
-        }, 7000);
-        return () => {
-            clearTimeout(timer);
-        };
-    }, [currentImageIndex]);
-
-    console.log(currentImageIndex);
-
-    let currentImage1 = images1[currentImageIndex];
-    let currentImage2 = images2[currentImageIndex];
-    let currentImage3 = images3[currentImageIndex];
     return (
         <Container>
             <ContainerAboutMe>
@@ -36,36 +19,23 @@ export const AboutUs = () => {
                 <Quote>
                     “От души душевно в душу. Не луивитон, но пацыки оценят”
                 </Quote>
-                <ComponentsContainer>
-                    <TransitionGroup component={null}>
-                        <CSSTransition
-                            key={currentImageIndex}
-                            timeout={5000}
-                            classNames="fade"
-                        >
-                            <GalleryContainer>
-                                <Gallery>
-                                    <GalleryItem
-                                        maxWidth={"542px"}
-                                        maxHeight={"724px"}
-                                        width={"47vw"}
-                                        height={"70vw"}
-                                        gridArea={"main"}
-                                        image={currentImage1}
-                                    />
-                                    <GalleryItem
-                                        image={currentImage2}
-                                        gridArea={"music"}
-                                    />
-                                    <GalleryItem
-                                        image={currentImage3}
-                                        gridArea={"quote"}
-                                    />
-                                </Gallery>
-                            </GalleryContainer>
-                        </CSSTransition>
-                    </TransitionGroup>
-                </ComponentsContainer>
+
+                <Gallery>
+                    <GalleryItem
+                        maxWidth={"542px"}
+                        maxHeight={"724px"}
+                        width={"47vw"}
+                        height={"70vw"}
+                        gridArea={"main"}
+                        images={images1}
+                        timer={4000}
+                    />
+                    <GalleryItem gridArea={"music"} images={images2}
+                    timer={4200} />
+                    <GalleryItem gridArea={"quote"} images={images3} 
+                    timer={4400}/>
+                </Gallery>
+
                 <Сontacts>
                     <СontactsIcon image={logo} size={"43vw"} />
                     <СontactsContainerInfo size={"43vw"}>
@@ -94,26 +64,6 @@ const Container = styled.div`
     justify-content: center;
 
     padding: 200px 0 4vw 0;
-`;
-const GalleryContainer = styled.div`
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translate(-50%);
-`;
-const ComponentsContainer = styled.div`
-    position: relative;
-    max-width: 972px;
-    width: calc(84vw + 30px);
-    max-height: 724px;
-    height: 70vw;
-    margin-top: 60px;
-
-    overflow: hidden;
-
-    @media (max-width: 1200px) {
-        width: 86vw;
-    }
 `;
 
 const ContainerAboutMe = styled.div`
@@ -157,46 +107,44 @@ const Gallery = styled.div`
     grid-template-areas:
         "main music"
         "main quote";
-
     grid-template-rows: 3fr 2fr;
     gap: 30px;
-
+    margin-top: 60px;
     @media (max-width: 1200px) {
         gap: 2vw;
     }
 `;
 
-type GalleryType = {
-    maxWidth?: string;
-    maxHeight?: string;
-    width?: string;
-    height?: string;
-    gridArea: "main" | "music" | "quote";
-    image: string;
-};
+// type GalleryType = {
+//     maxWidth?: string;
+//     maxHeight?: string;
+//     width?: string;
+//     height?: string;
+//     gridArea: "main" | "music" | "quote";
+//     image: string;
+// };
 
-const GalleryItem = styled.div<GalleryType>`
-    display: grid;
-    max-width: ${(props) => (props.maxWidth ? props.maxWidth : "400px")};
-    max-height: ${(props) => (props.maxHeight ? props.maxHeight : "")};
+// const GalleryItem = styled.div<GalleryType>`
+//     display: grid;
+//     max-width: ${(props) => (props.maxWidth ? props.maxWidth : "400px")};
+//     max-height: ${(props) => (props.maxHeight ? props.maxHeight : "")};
 
-    width: ${(props) => (props.width ? props.width : "37vw")};
-    height: ${(props) => (props.height ? props.height : "")};
+//     width: ${(props) => (props.width ? props.width : "37vw")};
+//     height: ${(props) => (props.height ? props.height : "")};
 
-    border-radius: 30px;
-    grid-area: ${(props) => props.gridArea};
+//     border-radius: 30px;
+//     grid-area: ${(props) => props.gridArea};
+//     background-color: #4d4d4d;
 
-    background-color: #4d4d4d;
+//     background-image: url(${(props) => props.image});
+//     background-size: cover;
+//     background-repeat: no-repeat;
+//     background-position: center;
 
-    background-image: url(${(props) => props.image});
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-
-    @media (max-width: 600px) {
-        border-radius: 20px;
-    }
-`;
+//     @media (max-width: 600px) {
+//         border-radius: 20px;
+//     }
+// `;
 
 const Сontacts = styled.div`
     display: flex;
